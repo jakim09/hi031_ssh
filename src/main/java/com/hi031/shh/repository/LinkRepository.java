f94a3c1d66bd711a37b2e9ac19c4d2a18f79ca93
minsun <kor3334@naver.com>
2021-10-03 16:13:22 +0900

    [Update] 링크 제안 수신, 알람

--- src/main/java/com/hi031/shh/repository/LinkRepository.java
+++ src/main/java/com/hi031/shh/repository/LinkRepository.java
@@ -12,12 +12,12 @@ import com.hi031.shh.domain.Link;
 
 @EnableJpaRepositories
 public interface LinkRepository extends PagingAndSortingRepository<Link, Integer>, CrudRepository<Link, Integer>  {
-	List<Link> findByLinkId(String linkId) throws DataAccessException;
+	Link findByLinkId(String linkId) throws DataAccessException;
 	List<Link> findByProposerId(String proposerId) throws DataAccessException;
 	List<Link> findByReceiverId(String receiverId) throws DataAccessException;
 	public long countByProposerId(String proposerId) throws DataAccessException;
 	Link findByProposerIdAndReceiverId(String proposerId, String receiverId) throws DataAccessException;
 	List<Link> findLinkByReceiverIdAndStateAndManagement(String receiverId, int state, int managemnet) throws DataAccessException;
 	List<Link> findLinkByProposerIdAndStateAndManagement(String proposerId, int state, int managemnet) throws DataAccessException;
-
+	List<Link> findTop7ByIsWatchedAndReceiverIdOrderByProposalDateDesc(int isWatched, String ReceiverId) throws DataAccessException;
 }
--- src/main/java/com/hi031/shh/service/ShhImpl.java
+++ src/main/java/com/hi031/shh/service/ShhImpl.java
@@ -213,7 +213,10 @@ public class ShhImpl implements ShhFacade {
 	}
 	
 	@Override
-	public Link updateLink(Link link) {
+	public Link updateLink(Link link, int state, int management) {
+		link.setState(state);
+		link.setManagement(management);
+		
 		return linkRepo.save(link);
 	}
 
@@ -307,6 +310,11 @@ public class ShhImpl implements ShhFacade {
 	}
 	
 	@Override
+	public Link getLinkByLinkId(String linkId) {
+		return linkRepo.findByLinkId(linkId);
+	}
+	
+	@Override
 	public long countByProposerId(String proposerId) {
 		return linkRepo.countByProposerId(proposerId);
 	}
@@ -346,6 +354,9 @@ public class ShhImpl implements ShhFacade {
 		return consumerCouponRepo.save(coupon);
 	}
 
-
-
+	@Override
+	public List<Link> getLinkAlarm(int isWatched, String storeId) {
+		List<Link> list = linkRepo.findTop7ByIsWatchedAndReceiverIdOrderByProposalDateDesc(isWatched, storeId);
+		return list;
+	}
 }
